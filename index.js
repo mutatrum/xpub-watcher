41c9ba3369722e6af818e906011f9855522c2796
mutatrum <mutatrum@gmail.com>
2020-07-14 17:42:10 +0200

    Rename entry point

--- index.js
+++
@@ -1,3 +0,0 @@
-const config = require('./config.json');
-const XpubWatcher = require('./src/xpub-watcher.js');
-new XpubWatcher(config).run();
---
+++ xpub-watcher.js
@@ -0,0 +1,3 @@
+const config = require('./config.json');
+const Main = require('./src/main.js');
+new Main(config).run();
